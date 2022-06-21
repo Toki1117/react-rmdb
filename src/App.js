1431ed3b12a855fcd0494a349f71f3bd3289ceb6
Diana Salguero <dmsq2017@outlook.com>
2022-06-20 18:29:30 -0600

    💅global style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+// Styles
+// it's a wrapping div with general style
+import { GlobalStyle } from "./GlobalStyle";
+
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle/>
     </div>
   );
 }
