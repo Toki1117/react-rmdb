d4e444be6bf016e3a8fc8b7af7be954b08fd6bf9
Diana Salguero <dmsq2017@outlook.com>
2022-06-28 17:03:00 -0600

    ✨hero image component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
-
+import HeroImage from "./HeroImage";
 // Hooks
 import {useHomeFetch} from '../hooks/useHomeFetch';
 
@@ -12,8 +12,19 @@ import NoImage from "../images/no_image.jpg";
 const Home = () => {
     const {state, loading, error} = useHomeFetch();
     console.log({state});
-
-    return <div>Home Page</div>
+    return (
+        <>
+            {
+                state?.results[0] 
+                ? (<HeroImage 
+                    image={`${IMAGE_BASE_URL}${ BACKDROP_SIZE }${state.results[0].backdrop_path}`}
+                    title={state.results[0].original_title}
+                    text={state.results[0].overview}
+                />) 
+                : null
+            }
+        </>
+    );
 };
 
 export default Home;
