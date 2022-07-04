180e70198441d383246836b3af7185f5e5806987
Diana Salguero <dmsq2017@outlook.com>
2022-07-04 14:22:08 -0600

    ♻ refactoring SearchBar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,10 +1,11 @@
-import React, {useEffect, useRef, useState} from 'react';
+// import React, {useEffect, useRef, useState} from 'react';
+import React, { Component } from 'react';
 //Image
 import icon from "../../images/search-icon.svg";
 // Styles
 import { Wrapper, Content } from "./SearchBar.styles";
 
-const SearchBar = ({setsearchTerm}) => {
+/* const SearchBar = ({setsearchTerm}) => {
     const [state, setState] = useState('');
     const initial = useRef(true);
 
@@ -34,6 +35,42 @@ const SearchBar = ({setsearchTerm}) => {
             </Content>
         </Wrapper>
     );
-};
+}; */
+
+class SearchBar extends Component {
+    state = { value: '' };
+    timer = null;
+
+    componentDidUpdate(_prevProps, prevState) {
+        const { value } = this.state;
+        if(value !== prevState.value) {
+            const { setsearchTerm } = this.props;
+
+            clearTimeout(this.timer);
+
+            this.timer = setTimeout(()=> {
+                setsearchTerm(value);
+            }, 500);
+        }
+    }
+
+    render() {
+        const { value } = this.state;
+        return (
+            <Wrapper>
+                <Content>
+                    <img src={icon} alt='search-icon' />
+                    <input 
+                        type='text'
+                        onChange={event => this.setState({ value: event.currentTarget.value })}
+                        value={value}
+                        placeholder='Search Movie' />
+                </Content>
+            </Wrapper>
+        );
+    }
+
+
+}
 
 export default SearchBar;
